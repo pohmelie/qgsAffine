311254582406de60688f4d60d0bc9926d10d8888
Nikita Melentev <Nikita.Melentev@kronshtadt.ru>
2019-02-07 17:07:45 +0300

    remove obsolete functions

--- qgsAffine/__init__.py
+++ qgsAffine/__init__.py
@@ -22,26 +22,6 @@
 """
 
 
-def name():
-    return "qgsAffine"
-
-
-def description():
-    return "Apply affine transformations to selected geometries."
-
-
-def version():
-    return "Version 2.0.2"
-
-
-def icon():
-    return "icon.svg"
-
-
-def qgisMinimumVersion():
-    return "3.4"
-
-
 def classFactory(iface):
     from .module import AffineUI
     return AffineUI(iface)
